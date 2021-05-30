403a6adc1b1478f94384f2348ddf8d2ec7b82c5c
Leslie <Leslie@users-MBP.lan1>
2021-05-29 19:56:17 -0500

    num sorter latest

--- NumberSorter.js
+++ NumberSorter.js
@@ -7,19 +7,19 @@ function getAscending(){
     alert("Please provide an input");
     return;
   }
-  var b = inputVal.split(' ').map(function(item) {
+  var res = inputVal.split(' ').map(function(item) {
     return parseFloat(item, 10);
   });
-  for (var x = 0; x < b.length; x++) {
-    if (isNaN(b[x])) {
+  for (var x = 0; x < res.length; x++) {
+    if (isNaN(res[x])) {
       alert("Please enter only numbers!");
       return;
     }
   }
-  b.sort();
+  res.sort(function(a, b){return a-b});
   // Displaying the value
   var z = document.createElement('p'); // is a node
-  z.innerHTML = b;
+  z.innerHTML = res;
   parent.appendChild(z);
 }
 function getDescending(){
